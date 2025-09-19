6d7d92d545b394a24f223cdf202f6f65f276e712
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-09-19 06:31:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Countries from './components/countries/countries'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     
+     <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const countries = () => {
+    return (
+        <div>
+            <h1>in tha countries</h1>
+        </div>
+    );
+};
+
+export default countries;
